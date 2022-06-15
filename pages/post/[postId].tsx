4c4445b27d86deba2f7365d09fb1c35db3852414
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-06-15 16:23:24 +0530

    feat: :speech_balloon: comments UI

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,21 +1,66 @@
 import { useQuery } from '@apollo/client'
+import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import Loader from '../../components/Loader'
 import Post from '../../components/Post'
 import { GET_POST_BY_POST_ID } from '../../graphql/queries'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type FormData = {
+  comment: string
+}
 
 function PostPage() {
   const router = useRouter()
+  const { data: session } = useSession()
   const { data } = useQuery(GET_POST_BY_POST_ID, {
     variables: {
       id: router.query.postId
     }
   })
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors }
+  } = useForm<FormData>()
+
+  const onSubmit: SubmitHandler<FormData> = async data => {
+    console.log(data)
+  }
+
   const post: Post = data?.getPost
+  if (!post) return <Loader />
 
   return (
     <div className="mx-auto my-7 max-w-5xl">
       <Post post={post} />
+
+      <div className="-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
+        <p className="text-sm mb-4">
+          Comment as <span className="text-red-500">{session?.user?.name}</span>
+        </p>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2"
+        >
+          <textarea
+            {...register('comment')}
+            disabled={!session}
+            className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
+            placeholder={session ? `What are your thoughts` : `Please Sign In`}
+          />
+          <button
+            type="submit"
+            className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200"
+          >
+            Comment
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
